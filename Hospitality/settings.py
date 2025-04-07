52235907ce0dc62a202ef7a006f45893b55ed559
tanmay1413 <tanmaykulkarni1413@gmail.com>
2025-04-07 18:39:39 +0530

    minor changes

--- Hospitality/settings.py
+++ Hospitality/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rentper_hospitality',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
