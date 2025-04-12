0e67f7f131a3054eed3a16c61f096b730c9b3b6e
tanmay1413 <tanmaykulkarni1413@gmail.com>
2025-04-12 14:36:08 +0530

    update settings

--- Hospitality/settings.py
+++ Hospitality/settings.py
@@ -202,15 +202,6 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 
-print(EMAIL_HOST_USER) 
-print(EMAIL_HOST_PASSWORD) 
-
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'rentperhospitality@gmail.com' # email sending address
-# EMAIL_HOST_PASSWORD = 'zyan jxol ypto qtow'
 
 # Authenticate if local account with this email address already exists
 SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
